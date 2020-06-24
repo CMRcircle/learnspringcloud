c0e510c802aa17818a2fcb626cd4c6b2cb3c13c5
chengmr <762727761@qq.com>
2020-06-24 14:37:39 +0800

    Spring Cloud Bus

---
+++ sc-f-chapter8/config-client/src/test/java/com/forezp/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.forezp.configclient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
