ada67e1afb3c3ea020e64a4e7493a292e507c5d9
chengmr <762727761@qq.com>
2020-06-24 16:42:59 +0800

    Sleuth

---
+++ sc-f-chapter9/service-miya/src/test/java/com/forezp/servicemiya/ServiceMiyaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.forezp.servicemiya;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMiyaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
