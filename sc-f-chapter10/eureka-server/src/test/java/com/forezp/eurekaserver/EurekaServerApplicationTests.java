f2819827c89988f466a9d91d6c5da4b1e547c004
chengmr <762727761@qq.com>
2020-06-24 17:57:55 +0800

    高可用的服务注册中心

---
+++ sc-f-chapter10/eureka-server/src/test/java/com/forezp/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.forezp.eurekaserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
---
+++ sc-f-chapter10/service-hi/src/test/java/com/forezp/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.forezp.servicehi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
